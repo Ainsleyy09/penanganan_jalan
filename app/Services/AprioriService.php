348510edc9cbe1c94fccd5549c86612374433908
Aisyah <162683506+Ainsleyy09@users.noreply.github.com>
2026-06-22 02:24:18 +0700

    Update AprioriService.php

--- app/Services/AprioriService.php
+++ app/Services/AprioriService.php
@@ -6,7 +6,9 @@ use App\Models\Segment;
 
 class AprioriService
 {
-    shell_exec(
+    public function process()
+    {
+        shell_exec(
             "python "
                 . base_path('python/apriori.py')
                 . " 2>&1"
