c488413e026b230bf60c77c64791b84dc104128b
Aisyah <162683506+Ainsleyy09@users.noreply.github.com>
2026-06-22 02:19:10 +0700

    Update AprioriService.php

--- app/Services/AprioriService.php
+++ app/Services/AprioriService.php
@@ -8,12 +8,12 @@ class AprioriService
 {
     public function process()
     {
-        shell_exec(
-            "python "
-                . base_path('python/apriori.py')
-                . " 2>&1"
-        );
+         $output = shell_exec(
+        "python3 "
+        . base_path('python/apriori.py')
+        . " 2>&1"
+    );
 
-        return true;
+    dd($output);
     }
 }
